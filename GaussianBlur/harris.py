50e878e0ce64b910a1df9db5ff3cd3279affd98e
Dengjianping <djptux@gmail.com>
2017-02-22 23:54:07 +0800

    finished

--- GaussianBlur/harris.py
+++ GaussianBlur/harris.py
@@ -37,11 +37,13 @@ def gaussianBlur(input, radius, theta):
 
 def gradientX(input):
     kernel = np.array([[1, -1]])
+    # kernel = np.array([[-1, 0, 1],[-1,0,1],[-1,0,1]])
     gradient = convolve2D(input, kernel)
     return gradient
 
 def gradientY(input):
     kernel = np.array([[1], [-1]])
+    # kernel = np.array([[-1,-1,-1],[0,0,0], [1,1,1]])
     gradient = convolve2D(input, kernel)
     return gradient
 
@@ -55,20 +57,72 @@ def blurGradientXY(lx, ly, radius, theta):
     gradient = gaussianBlur(gradient, radius, theta)
     return gradient
 
-def maxR(image, gradientXX, gradientYY, gradientXY, k):
+def r(gradientXX, gradientYY, gradientXY, i, j, k):
+    a = [-2,-1,0,1, 2]
+    window = np.zeros((5,5), dtype='float32')
+    for m in a:
+        for n in a:
+            if (m!=0 and n!=0):
+                z = np.array([[gradientXX[i+m][j+n], gradientXY[i+m][j+n]], [gradientXY[i+m][j+n], gradientYY[i+m][j+n]]])
+                t = linalg.det(z) - k * (np.trace(z) ** 2)
+                window[m][n] = t
+    return np.max(window)
+
+def maxRv(image, gradientXX, gradientYY, gradientXY, k, repression, window):
     row, col = image.shape
-    max = 0
+    maxR = 0
     for i in range(row):
         for j in range(col):
             m = np.array([[gradientXX[i][j], gradientXY[i][j]], [gradientXY[i][j], gradientYY[i][j]]])
             t = linalg.det(m) - k*(np.trace(m)**2)
-            if (max < t):
-                max = t
-
+            if (maxR < t):
+                maxR = t
+    point = []
     for i in range(row):
         for j in range(col):
-            m = np.array([[gradientXX[i][j], gradientXY[i][j]], [gradientXY[i][j], gradientYY[i][j]]])
-            eigen = linalg.eig(m)[0]
+            if (window-1 < i < row-int(window/2) and window-1 < j < col -int(window/2)):
+                m = np.array([[gradientXX[i][j], gradientXY[i][j]], [gradientXY[i][j], gradientYY[i][j]]])
+                # eigen = linalg.eig(m)[0]
+                t = linalg.det(m) - k * (np.trace(m) ** 2)
+                if (t > repression*maxR and t > r(gradientXX, gradientYY, gradientXY, i,j,k)):
+                    cor = (i, j)
+                    point.append(cor)
+    return point
 
 if __name__ == '__main__':
-    pass
+    radius = 3
+    theta = 2.0
+
+    k = 0.06
+    repression = 0.01
+    window = 5
+
+    path = r'image.jpg'
+    title = r'cv'
+
+    img = cv2.imread(path, 2)
+    #img = None
+    #cv2.cvtColor(image, img, cv2.COLOR_BGR2GRAY)
+    x = gradientX(img)
+    print(1)
+    y = gradientY(img)
+    print(2)
+    xx = blurGradientPower(x,radius, theta)
+    print(3)
+    yy = blurGradientPower(y,radius, theta)
+    print(4)
+    xy = blurGradientXY(x,y, radius, theta)
+    print(5)
+    point = maxRv(img, xx,yy,xy, k, repression, window)
+    a = [ -1, 0, 1]
+    for p in point:
+        print(p)
+        i, j = p
+        for p in a :
+            for q in a:
+                img[i+p][j+q] = 188
+
+    title = 'harris'
+    cv2.namedWindow(title)
+    cv2.imshow(title, img)
+    cv2.waitKey(0)
